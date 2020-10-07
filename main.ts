1068e47926b343fb90fe484d2879e87393fac910
mfilipczak <michal@filipczak.info>
2020-10-07 11:58:08 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,17 @@
 radio.onReceivedNumber(function (receivedNumber) {
     instrukcja = receivedNumber
     if (instrukcja == 0) {
-        speed = 0
         maqueen.motorStop(maqueen.Motors.All)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     } else if (instrukcja == 1) {
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        maqueen.motorStop(maqueen.Motors.M1)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
     } else if (instrukcja == 2) {
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-        maqueen.motorStop(maqueen.Motors.M2)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
     } else if (instrukcja == 3) {
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
@@ -24,9 +23,11 @@ radio.onReceivedNumber(function (receivedNumber) {
         maqueen.motorStop(maqueen.Motors.All)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, speed)
     } else if (instrukcja == 5) {
-        speed = speed + 10
+        speed = Math.max(speed + 25, 255)
+        basic.showString("" + (speed))
     } else if (instrukcja == 6) {
-        speed = speed - 10
+        speed = Math.min(speed - 25, 0)
+        basic.showString("" + (speed))
     } else if (instrukcja == 7) {
         music.playTone(247, music.beat(BeatFraction.Whole))
     } else {
@@ -38,10 +39,9 @@ let instrukcja = 0
 let speed = 0
 radio.setGroup(24)
 speed = 0
-music.playTone(247, music.beat(BeatFraction.Double))
 basic.forever(function () {
     odl = maqueen.Ultrasonic(PingUnit.Centimeters)
     if (odl < 5) {
-        speed = 0
+        maqueen.motorStop(maqueen.Motors.All)
     }
 })
